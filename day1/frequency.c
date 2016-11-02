97477e94af52100bc282d3c8118f70a987620c69
Daniel Ems <danielems3@outlook.com>
2016-11-02 09:28:17 -0400

    Add additional counter to while loop to properly create mulit array GOOD JOB!. Not finished yet. introduced strtok to split on dilemiters, simplifying while loop and adding flexibility. Similar to .strip in python

--- day1/frequency.c
+++ day1/frequency.c
@@ -1,32 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 
-char words[100][15];
 
 int main(void)
 {
+	
+	
+	char words[100][100];
 
 	FILE *wordlist = fopen("wordlist", "r");
 	
-	int a;
-	for(a =0; 
-	fgets(words[][], sizeof(words), wordlist);
-
-	printf("%s", words);
 	
-	int end = strlen(words);
+	char buf[100];
+	fgets(buf, sizeof(buf), wordlist);
+	char *temp = strtok(buf, " .");
+	
+	while(temp){
+	printf("%s\n", temp);
+	temp = strtok(NULL, " .");
+	}
+
+	
+
+	//printf("%c", buf[0]);
+	
+	int end = strlen(buf);
+	
 	
 	int a = 0;
+	int b;
+	int count;
 	while(a < end){
-		if(words[a] == ' '){
-		printf("\n");
+		
+		if(buf[a] == ' '){
+			count++;
+			words[count][b] = '\0';
+			b = 0;
+			printf("\n");
 		}
 		else {
-		printf("%c", words[a]);
+			words[count][b] = buf[a];
+			printf("buf a: %c\tcount: %d\tb: %d\n", buf[a], count, b);
+			b++;
 		}
-		a++;
+			a++;
 		}
 
+	printf("%s\n", words[6]);
 }
 	
 	
