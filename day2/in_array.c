cf4dba839d25e933b963cdd682845f909050d67c
Daniel Ems <danielems3@outlook.com>
2016-11-02 15:49:38 -0400

    Initiate and print array

---
+++ day2/in_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool in_array(int array[10]);
+int main(void)
+{
+	
+	int a = 0;
+	while(a<5)
+	{
+		int array [5] = {1,2,3};
+		printf("%d\n", array[a]);
+		a++;
+	}
+}
+
+
+	
